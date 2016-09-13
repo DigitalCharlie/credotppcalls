97384a80d27c9a29c3f3de8d74413ee666f14581
Digital Charlie <furman.jr@gmail.com>
2016-09-13 15:25:06 -0700

    Moving to this directory

---
+++ js/repdata.js
@@ -0,0 +1,29 @@
+var rep_table= [
+    {
+    	first_name: "Don",
+    	last_name: "Young",
+    	bioguide_id: "Y000033",
+    	disposition: "Undecided",
+    	whipcount_id: "7641",
+    	phone: "",
+    },
+
+    {
+    	first_name: "David",
+    	last_name: "Jolly",
+    	bioguide_id: "J000296",
+    	disposition: "Undecided",
+    	whipcount_id: "7642",
+    	phone: "",
+
+    },
+
+    {
+    	first_name: "Bradley",
+    	last_name: "Byrne",
+    	bioguide_id: "B001289",
+    	disposition: "Undecided",
+    	whipcount_id: "7643",
+    	phone: "",
+    },
+]
