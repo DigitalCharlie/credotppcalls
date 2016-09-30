d0b2046858dad0d82b87e965fbf9ad4c8f69b319
Digital Charlie <furman.jr@gmail.com>
2016-09-30 12:08:10 -0700

    ?

--- js/core.js
+++ js/core.js
@@ -88,7 +88,7 @@ function populateRep(rep) {
 	$('#overlay-content-multiple-reps').addClass('hidden');
 	$('#ak-form input[type="radio"]').prop('name', 'response-' + rep.whipcount_id);
 	$('#rep-radio-button-error').prop('for', 'response-' + rep.whipcount_id);
-	var repResponse = 'response-' + rep.whipcount_id;
+	var repResponse = 'response-' + var rep.whipcount_id;
 }
 
 
